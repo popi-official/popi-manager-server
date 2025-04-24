c680b243b7e005a86ce7d3a1e5a010aaa440178f
현태 최 <ht3064@naver.com>
2025-04-24 22:41:11 +0900

    init: initial commit

---
+++ src/main/java/com/lgcns/PopiManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopiManagerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopiManagerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcns/PopiManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopiManagerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

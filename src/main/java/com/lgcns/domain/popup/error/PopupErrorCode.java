f2fce0869d502d93b96a4bd0e2599fe3c63a7d23
jeongyeon0208 <githubb0208@gmail.com>
2025-04-25 15:39:15 +0900

    [LCR-16] chore: popup domain 구조 설정

---
+++ src/main/java/com/lgcns/domain/popup/error/PopupErrorCode.java
@@ -0,0 +1,21 @@
+package com.lgcns.domain.popup.error;
+
+import com.lgcns.global.error.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum PopupErrorCode implements ErrorCode {
+
+    POPUP_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 팝업이 존재하지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getErrorName() {
+        return this.name();
+    }
+}

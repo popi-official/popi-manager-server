2114551731a34bdd61a316fadaffdfe8b3469316
조영상 <51320595+0ssang@users.noreply.github.com>
2025-04-26 00:43:58 +0900

    [LCR-41] feat: 운영자 error 코드 구현

--- src/main/java/com/lgcns/domain/manager/domain/Manager.java
+++ src/main/java/com/lgcns/domain/manager/domain/Manager.java
@@ -1,5 +1,6 @@
 package com.lgcns.domain.manager.domain;
 
+import com.lgcns.global.model.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Manager {
+public class Manager extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "manager_id")
---
+++ src/main/java/com/lgcns/domain/manager/exception/ManagerErrorCode.java
@@ -0,0 +1,22 @@
+package com.lgcns.domain.manager.exception;
+
+import com.lgcns.global.error.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ManagerErrorCode implements ErrorCode {
+    DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 사용 중인 사용자 이름입니다."),
+    INVALID_PASSWORD_FORMAT(HttpStatus.BAD_REQUEST, "비밀번호 형식이 올바르지 않습니다."),
+    INVALID_USERNAME_FORMAT(HttpStatus.BAD_REQUEST, "사용자 이름 형식이 올바르지 않습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public String getErrorName() {
+        return this.name();
+    }
+}

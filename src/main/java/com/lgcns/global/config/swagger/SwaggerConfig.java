6db4a1bb74aea5a49944de8693e0a55c855cbe57
woogieon8on <swp0927@naver.com>
2025-04-25 15:45:47 +0900

    [LCR-46] chore: Swagger 초기 설정 추가

---
+++ src/main/java/com/lgcns/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,64 @@
+package com.lgcns.global.config.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class SwaggerConfig {
+
+    // private final SpringEnvironmentHelper springEnvironmentHelper;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("PoPI Manager Server API")
+                                .description("PoPI Manager Server API 명세서입니다.")
+                                .version("v0.0.1"))
+                // .servers(getSwaggerServers())
+                .components(authSetting())
+                .addSecurityItem(securityRequirement());
+    }
+
+    /*
+    private List<Server> getSwaggerServers() {
+        return List.of(new Server().url(getServerUrlByProfile()));
+    }
+
+    private String getServerUrlByProfile() {
+        return switch (springEnvironmentHelper.getCurrentProfile()) {
+            case "prod" -> UrlConstants.PROD_SERVER_URL;
+            case "dev" -> UrlConstants.DEV_SERVER_URL;
+            default -> UrlConstants.LOCAL_SERVER_URL;
+        };
+    }
+    */
+
+    private Components authSetting() {
+        return new Components()
+                .addSecuritySchemes(
+                        "accessToken",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization"));
+    }
+
+    private SecurityRequirement securityRequirement() {
+        SecurityRequirement securityRequirement = new SecurityRequirement();
+        securityRequirement.addList("accessToken");
+        return securityRequirement;
+    }
+}

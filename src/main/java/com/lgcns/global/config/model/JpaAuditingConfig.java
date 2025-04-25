c6abad018dc76ec5aa071ae90bfa88d5bcfb2161
woogieon8on <140885810+woogieon8on@users.noreply.github.com>
2025-04-25 17:36:47 +0900

    feat: BaseTimeEntity 생성 (#5)

---
+++ src/main/java/com/lgcns/global/config/model/JpaAuditingConfig.java
@@ -0,0 +1,8 @@
+package com.lgcns.global.config.model;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {}

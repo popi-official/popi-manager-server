43fd676684b3c41f6b4cb1be4825f08d849c02c1
woogieon8on <swp0927@naver.com>
2025-04-26 16:08:07 +0900

    [LCR-20] feat: Item 엔티티 생성

---
+++ src/main/java/com/lgcns/domain/item/domain/Item.java
@@ -0,0 +1,54 @@
+package com.lgcns.domain.item.domain;
+
+import com.lgcns.global.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private String price;
+
+    private String imageUrl;
+
+    private int qty;
+
+    private int minQty;
+
+    private String displayStand;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    public Item(
+            String name, String price, String imageUrl, int qty, int minQty, String displayStand) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.qty = qty;
+        this.minQty = minQty;
+        this.displayStand = displayStand;
+    }
+
+    public static Item createItem(
+            String name, String price, String imageUrl, int qty, int minQty, String displayStand) {
+        return Item.builder()
+                .name(name)
+                .price(price)
+                .imageUrl(imageUrl)
+                .qty(qty)
+                .minQty(minQty)
+                .displayStand(displayStand)
+                .build();
+    }
+}
---
+++ src/main/java/com/lgcns/domain/item/repository/ItemRepository.java
@@ -0,0 +1,6 @@
+package com.lgcns.domain.item.repository;
+
+import com.lgcns.domain.item.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {}

16bb406d30f1ded2785a911ac4b7792d87790198
현태 최 <ht3064@naver.com>
2025-04-28 17:47:39 +0900

    [LCR-65] chore: redis config 추가

---
+++ src/main/java/com/lgcns/infra/redis/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.lgcns.infra.redis;
+
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfig =
+                new RedisStandaloneConfiguration(redisProperties.host(), redisProperties.port());
+        if (!redisProperties.password().isBlank()) {
+            redisStandaloneConfig.setPassword(redisProperties.password());
+        }
+
+        LettuceClientConfiguration lettuceClientConfig =
+                LettuceClientConfiguration.builder()
+                        .commandTimeout(Duration.ofSeconds(1))
+                        .shutdownTimeout(Duration.ZERO)
+                        .build();
+
+        return new LettuceConnectionFactory(redisStandaloneConfig, lettuceClientConfig);
+    }
+}

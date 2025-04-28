a298d3f8a25452f1b42671daa63e42d0a1498ff2
woogieon8on <swp0927@naver.com>
2025-04-28 17:15:41 +0900

    [LCR-63] feat: preSigned URL 발급 기능 구현

---
+++ src/main/java/com/lgcns/infra/s3/dto/request/FileNameRequest.java
@@ -0,0 +1,9 @@
+package com.lgcns.infra.s3.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+@Schema(description = "S3 업로드 파일 이름 DTO")
+public record FileNameRequest(
+        @NotBlank(message = "파일 이름은 필수입니다.") @Schema(description = "파일 이름", example = "popi")
+                String fileName) {}

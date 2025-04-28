6e364317aeae8d165872219e44ad293164f5d895
jeongyeon0208 <githubb0208@gmail.com>
2025-04-28 16:35:03 +0900

    [LCR-51] chore: flyway init sql 파일 생성

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE manager (
+                         manager_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         username VARCHAR(255) NOT NULL,
+                         password VARCHAR(255) NOT NULL
+);

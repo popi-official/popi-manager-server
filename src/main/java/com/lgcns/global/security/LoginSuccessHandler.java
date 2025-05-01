f182dbeb1af2adea2031ca9259fa784d8b5d379f
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-02 04:12:58 +0900

    [LCR-14] feat: 로그인 성공 핸들러 구현

---
+++ src/main/java/com/lgcns/global/security/LoginSuccessHandler.java
@@ -0,0 +1,42 @@
+package com.lgcns.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lgcns.domain.auth.domain.PrincipalDetails;
+import com.lgcns.domain.auth.dto.response.LoginResponse;
+import com.lgcns.domain.auth.service.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+    private static final String RESPONSE_CONTENT_TYPE = "application/json;charset=utf-8";
+    private final ObjectMapper objectMapper;
+    private final TokenService tokenService;
+
+    @Override
+    public void onAuthenticationSuccess(
+            HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType(RESPONSE_CONTENT_TYPE);
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+        String accessToken =
+                tokenService.createAccessToken(
+                        principalDetails.getUsername(), principalDetails.getRole());
+        String refreshToken =
+                tokenService.createRefreshToken(
+                        principalDetails.getUsername(), principalDetails.getId());
+
+        LoginResponse loginResponse = LoginResponse.of(accessToken, refreshToken);
+
+        objectMapper.writeValue(response.getWriter(), loginResponse);
+    }
+}

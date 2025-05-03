2226fa2721e9759dea272501cef309c506216657
현태 최 <ht3064@naver.com>
2025-05-03 18:42:05 +0900

    [LCR-90] feat: 쿠키 유틸리티 구현

---
+++ src/main/java/com/lgcns/global/common/constants/SecurityConstants.java
@@ -0,0 +1,5 @@
+package com.lgcns.global.common.constants;
+
+public final class SecurityConstants {
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
+}
---
+++ src/main/java/com/lgcns/global/util/CookieUtil.java
@@ -0,0 +1,27 @@
+package com.lgcns.global.util;
+
+import static com.lgcns.global.common.constants.SecurityConstants.REFRESH_TOKEN_COOKIE_NAME;
+
+import org.springframework.boot.web.server.Cookie;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    public HttpHeaders generateRefreshTokenCookie(String refreshToken) {
+        ResponseCookie refreshTokenCookie =
+                ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, refreshToken)
+                        .path("/")
+                        .secure(true)
+                        .sameSite(Cookie.SameSite.NONE.attributeValue())
+                        .httpOnly(true)
+                        .build();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
+        return headers;
+    }
+}

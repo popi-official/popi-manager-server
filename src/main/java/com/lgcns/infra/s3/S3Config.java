fe0b3929ab1d3a24990371d3c22a694fadfb0cff
현태 최 <ht3064@naver.com>
2025-05-08 14:58:43 +0900

    [LCR-63] feat: Amazon S3 클라이언트 설정을 위한 S3Config 추가

---
+++ src/main/java/com/lgcns/infra/s3/S3Config.java
@@ -0,0 +1,31 @@
+package com.lgcns.infra.s3;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class S3Config {
+
+    private final S3Properties s3Properties;
+
+    @Bean
+    public AmazonS3 s3Client() {
+        BasicAWSCredentials credentials =
+                new BasicAWSCredentials(s3Properties.accessKey(), s3Properties.secretKey());
+        AwsClientBuilder.EndpointConfiguration endpointConfiguration =
+                new AwsClientBuilder.EndpointConfiguration(
+                        s3Properties.endpoint(), s3Properties.region());
+
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(endpointConfiguration)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}

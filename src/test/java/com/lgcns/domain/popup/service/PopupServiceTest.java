192585b96f8ca132a03a9b701e7ed01a54756ef9
jeongyeon0208 <githubb0208@gmail.com>
2025-05-08 15:00:52 +0900

    [LCR-24] test: 팝업 목록 조회 테스트 코드 작성

--- src/test/java/com/lgcns/domain/popup/service/PopupServiceTest.java
+++ src/test/java/com/lgcns/domain/popup/service/PopupServiceTest.java
@@ -9,6 +9,7 @@ import com.lgcns.domain.popup.domain.Popup;
 import com.lgcns.domain.popup.dto.request.PopupCreateRequest;
 import com.lgcns.domain.popup.dto.request.PopupWithChoicesCreateRequest;
 import com.lgcns.domain.popup.dto.response.PopupCreateResponse;
+import com.lgcns.domain.popup.dto.response.PopupPreviewResponse;
 import com.lgcns.domain.popup.repository.PopupRepository;
 import com.lgcns.domain.survey.domain.Choice;
 import com.lgcns.domain.survey.domain.Survey;
@@ -82,33 +83,52 @@ public class PopupServiceTest extends IntegrationTest {
             List<Choice> choiceList = choiceRepository.findAll();
             assertThat(choiceList).hasSize(16);
         }
+    }
 
-        private PopupWithChoicesCreateRequest createPopupWithChoicesCreateRequest() {
-            return new PopupWithChoicesCreateRequest(
-                    new PopupCreateRequest(
-                            "popup1",
-                            "https://bucket/asdf",
-                            LocalDate.parse("2025-01-01"),
-                            LocalDate.parse("2025-01-31"),
-                            LocalDateTime.parse("2025-01-01T10:00:00"),
-                            LocalDateTime.parse("2025-01-31T20:00:00"),
-                            LocalTime.parse("10:00:00"),
-                            LocalTime.parse("20:00:00"),
-                            100,
-                            20,
-                            "서울특별시 강남구 테헤란로 123",
-                            "3층 A호",
-                            37.123456,
-                            127.123456),
-                    List.of(
-                            new ChoiceCreateRequest(
-                                    List.of("choice1", "choice2", "choice3", "choice4")),
-                            new ChoiceCreateRequest(
-                                    List.of("choice1", "choice2", "choice3", "choice4")),
-                            new ChoiceCreateRequest(
-                                    List.of("choice1", "choice2", "choice3", "choice4")),
-                            new ChoiceCreateRequest(
-                                    List.of("choice1", "choice2", "choice3", "choice4"))));
+    @Nested
+    class 팝업_목록_조회 {
+        @Test
+        @Transactional
+        void 팝업_목록_성공한다() {
+            // given
+            PopupWithChoicesCreateRequest popupWithChoicesCreateRequest =
+                    createPopupWithChoicesCreateRequest();
+            popupService.createPopup(popupWithChoicesCreateRequest);
+            popupService.createPopup(popupWithChoicesCreateRequest);
+
+            // when
+            List<PopupPreviewResponse> popupPreviewResponseList = popupService.findAllPopups();
+
+            // then
+            assertThat(popupPreviewResponseList).hasSize(2);
         }
+    }
+
+    private PopupWithChoicesCreateRequest createPopupWithChoicesCreateRequest() {
+        return new PopupWithChoicesCreateRequest(
+                new PopupCreateRequest(
+                        "popup1",
+                        "https://bucket/asdf",
+                        LocalDate.parse("2025-01-01"),
+                        LocalDate.parse("2025-01-31"),
+                        LocalDateTime.parse("2025-01-01T10:00:00"),
+                        LocalDateTime.parse("2025-01-31T20:00:00"),
+                        LocalTime.parse("10:00:00"),
+                        LocalTime.parse("20:00:00"),
+                        100,
+                        20,
+                        "서울특별시 강남구 테헤란로 123",
+                        "3층 A호",
+                        37.123456,
+                        127.123456),
+                List.of(
+                        new ChoiceCreateRequest(
+                                List.of("choice1", "choice2", "choice3", "choice4")),
+                        new ChoiceCreateRequest(
+                                List.of("choice1", "choice2", "choice3", "choice4")),
+                        new ChoiceCreateRequest(
+                                List.of("choice1", "choice2", "choice3", "choice4")),
+                        new ChoiceCreateRequest(
+                                List.of("choice1", "choice2", "choice3", "choice4"))));
     }
 }

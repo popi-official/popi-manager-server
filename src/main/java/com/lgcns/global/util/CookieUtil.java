3ece164fc0c267ecd4f0774784a6183107f671e2
현태 최 <ht3064@naver.com>
2025-05-10 23:18:57 +0900

    [LCR-123] fix: 쿠키 domain 미설정으로 인한 미발급 문제 수정

--- src/main/java/com/lgcns/global/util/CookieUtil.java
+++ src/main/java/com/lgcns/global/util/CookieUtil.java
@@ -1,19 +1,26 @@
 package com.lgcns.global.util;
 
 import static com.lgcns.global.common.constants.SecurityConstants.REFRESH_TOKEN_COOKIE_NAME;
+import static com.lgcns.global.helper.SpringEnvironmentHelper.DEV;
 
+import com.lgcns.global.helper.SpringEnvironmentHelper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CookieUtil {
+
+    private final SpringEnvironmentHelper springEnvironmentHelper;
 
     public HttpHeaders generateRefreshTokenCookie(String refreshToken) {
         ResponseCookie refreshTokenCookie =
                 ResponseCookie.from(REFRESH_TOKEN_COOKIE_NAME, refreshToken)
                         .path("/")
+                        .domain(getCookieDomain())
                         .secure(true)
                         .sameSite(Cookie.SameSite.NONE.attributeValue())
                         .httpOnly(true)
@@ -23,5 +30,12 @@ public class CookieUtil {
         headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
         return headers;
+    }
+
+    private String getCookieDomain() {
+        return switch (springEnvironmentHelper.getCurrentProfile()) {
+            case DEV -> ".ceo.popi.today";
+            default -> "localhost";
+        };
     }
 }

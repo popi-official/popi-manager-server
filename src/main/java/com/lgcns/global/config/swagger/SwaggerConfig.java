3301aec3ef33b097309b704404b11ec141ec0fc1
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-05-13 00:30:26 +0900

    fix: dev 환경 CORS 및 쿠키 미발급 문제 수정 (#32)
    * [LCR-123] chore: 공통 URL 상수 클래스 생성
    
    * [LCR-123] chore: Spring 실행 프로파일 판별 헬퍼 클래스 추가
    
    * [LCR-123] fix: dev 환경에서 로컬 및 개발 도메인 CORS 허용 추가
    
    * [LCR-123] fix: 쿠키 domain 미설정으로 인한 미발급 문제 수정
    
    * [LCR-123] chore: 테스트 트리거 활성화
    
    * [LCR-123] chore: Swagger 문서 환경별 서버 URL 동적 설정
    
    * [LCR-123] fix: dev 환경 Swagger Try it out 동작을 위한 CORS 설정 추가
    
    * [LCR-123] chore: 테스트 트리거 제거

--- src/main/java/com/lgcns/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/lgcns/global/config/swagger/SwaggerConfig.java
@@ -1,10 +1,17 @@
 package com.lgcns.global.config.swagger;
 
+import static com.lgcns.global.common.constants.UrlConstants.DEV_SERVER_URL;
+import static com.lgcns.global.common.constants.UrlConstants.LOCAL_SERVER_URL;
+import static com.lgcns.global.helper.SpringEnvironmentHelper.DEV;
+
+import com.lgcns.global.helper.SpringEnvironmentHelper;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +22,8 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 public class SwaggerConfig {
 
+    private final SpringEnvironmentHelper springEnvironmentHelper;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
@@ -23,8 +32,20 @@ public class SwaggerConfig {
                                 .title("PoPI Manager Server API")
                                 .description("PoPI Manager Server API 명세서입니다.")
                                 .version("v0.0.1"))
+                .servers(getSwaggerServers())
                 .components(authSetting())
                 .addSecurityItem(securityRequirement());
+    }
+
+    private List<Server> getSwaggerServers() {
+        return List.of(new Server().url(getServerUrlByProfile()));
+    }
+
+    private String getServerUrlByProfile() {
+        return switch (springEnvironmentHelper.getCurrentProfile()) {
+            case DEV -> DEV_SERVER_URL;
+            default -> LOCAL_SERVER_URL;
+        };
     }
 
     private Components authSetting() {

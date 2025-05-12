1037a8e1e290d59771a5165bc490fae801913ef0
현태 최 <ht3064@naver.com>
2025-05-13 01:16:14 +0900

    [LCR-122] feat: 로그아웃 기능 구현

--- src/main/java/com/lgcns/domain/manager/controller/ManagerController.java
+++ src/main/java/com/lgcns/domain/manager/controller/ManagerController.java
@@ -2,6 +2,7 @@ package com.lgcns.domain.manager.controller;
 
 import com.lgcns.domain.manager.dto.request.ManagerCreateRequest;
 import com.lgcns.domain.manager.service.ManagerService;
+import com.lgcns.global.util.CookieUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -20,11 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ManagerController {
 
     private final ManagerService managerService;
+    private final CookieUtil cookieUtil;
 
     @PostMapping
     @Operation(summary = "운영자 계정 생성", description = "운영자가 직접 요청하지 않고, 개발자가 생성 후 전달하는 내부용 API입니다.")
     public ResponseEntity<Void> managerCreate(@RequestBody @Valid ManagerCreateRequest request) {
         managerService.createManager(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/logout")
+    @Operation(summary = "운영자 로그아웃", description = "운영자 로그아웃을 진행합니다.")
+    public ResponseEntity<Void> managerLogout() {
+        managerService.logoutManager();
+        return ResponseEntity.ok().headers(cookieUtil.deleteRefreshTokenCookie()).build();
     }
 }
--- src/main/java/com/lgcns/domain/manager/service/ManagerService.java
+++ src/main/java/com/lgcns/domain/manager/service/ManagerService.java
@@ -4,4 +4,6 @@ import com.lgcns.domain.manager.dto.request.ManagerCreateRequest;
 
 public interface ManagerService {
     void createManager(ManagerCreateRequest request);
+
+    void logoutManager();
 }

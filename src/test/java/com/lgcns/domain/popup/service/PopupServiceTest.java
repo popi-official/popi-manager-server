ecbbaa26d91203d9e5fbb45e08cce4ac335da0cb
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-13 00:12:54 +0900

    [LCR-127] test: 팝업 삭제 테스트 코드 작성

--- src/test/java/com/lgcns/domain/popup/service/PopupServiceTest.java
+++ src/test/java/com/lgcns/domain/popup/service/PopupServiceTest.java
@@ -1,8 +1,11 @@
 package com.lgcns.domain.popup.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.lgcns.IntegrationTest;
+import com.lgcns.domain.item.repository.ItemRepository;
+import com.lgcns.domain.item.service.ItemService;
 import com.lgcns.domain.manager.domain.Manager;
 import com.lgcns.domain.manager.repository.ManagerRepository;
 import com.lgcns.domain.popup.domain.Popup;
@@ -10,13 +13,15 @@ import com.lgcns.domain.popup.dto.request.PopupCreateRequest;
 import com.lgcns.domain.popup.dto.request.PopupWithChoicesCreateRequest;
 import com.lgcns.domain.popup.dto.response.PopupCreateResponse;
 import com.lgcns.domain.popup.dto.response.PopupPreviewResponse;
+import com.lgcns.domain.popup.exception.PopupErrorCode;
 import com.lgcns.domain.popup.repository.PopupRepository;
 import com.lgcns.domain.survey.domain.Choice;
 import com.lgcns.domain.survey.domain.Survey;
 import com.lgcns.domain.survey.dto.request.ChoiceCreateRequest;
 import com.lgcns.domain.survey.repository.ChoiceRepository;
 import com.lgcns.domain.survey.repository.SurveyRepository;
-import com.lgcns.global.security.PrincipalDetails;
+import com.lgcns.global.error.exception.CustomException;
+import jakarta.persistence.EntityManager;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -26,30 +31,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 
 public class PopupServiceTest extends IntegrationTest {
     @Autowired PopupService popupService;
+    @Autowired ItemService itemService;
     @Autowired PopupRepository popupRepository;
     @Autowired SurveyRepository surveyRepository;
 
     @Autowired private ManagerRepository managerRepository;
     @Autowired private ChoiceRepository choiceRepository;
+    @Autowired ItemRepository itemRepository;
 
-    private Manager manager;
+    @Autowired EntityManager entityManager;
+
+    private Manager ownerManager;
+    private Manager otherManager;
+    private Popup popup;
 
     @BeforeEach
     void setUp() {
-        manager = managerRepository.save(Manager.createManager("testUsername", "testPassword"));
+        ownerManager =
+                managerRepository.save(Manager.createManager("testUsername", "testPassword"));
+        otherManager =
+                managerRepository.save(Manager.createManager("otherManager", "testPassword"));
 
-        UserDetails userDetails = new PrincipalDetails(manager.getId(), manager.getRole(), null);
-        UsernamePasswordAuthenticationToken token =
-                new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(token);
+        setAuthentication(ownerManager);
     }
 
     @Nested
@@ -65,20 +72,20 @@ public class PopupServiceTest extends IntegrationTest {
             PopupCreateResponse response = popupService.createPopup(popupWithChoicesCreateRequest);
 
             // then
-            Popup popup = popupRepository.findAll().get(0);
+            Popup savedPopup = popupRepository.findById(response.popupId()).orElseThrow();
             Assertions.assertAll(
-                    () -> assertThat(response.popupId()).isEqualTo(popup.getId()),
-                    () -> assertThat(popup.getName()).isEqualTo("popup1"),
-                    () -> assertThat(popup.getImageUrl()).isEqualTo("https://bucket/asdf"),
+                    () -> assertThat(response.popupId()).isEqualTo(savedPopup.getId()),
+                    () -> assertThat(savedPopup.getName()).isEqualTo("popup1"),
+                    () -> assertThat(savedPopup.getImageUrl()).isEqualTo("https://bucket/asdf"),
                     () ->
-                            assertThat(popup.getPopupStartDate())
+                            assertThat(savedPopup.getPopupStartDate())
                                     .isEqualTo(LocalDate.parse("2025-01-01")),
                     () ->
-                            assertThat(popup.getPopupEndDate())
+                            assertThat(savedPopup.getPopupEndDate())
                                     .isEqualTo(LocalDate.parse("2025-01-31")));
 
             List<Survey> surveyList = surveyRepository.findAll();
-            assertThat(surveyList).hasSize(4);
+            assertThat(surveyList).hasSize(4); // 기존 팝업 + 새로 생성된 팝업의 설문들
 
             List<Choice> choiceList = choiceRepository.findAll();
             assertThat(choiceList).hasSize(16);
@@ -89,21 +96,61 @@ public class PopupServiceTest extends IntegrationTest {
     class 팝업_목록_조회 {
         @Test
         @Transactional
-        void 팝업_목록_성공한다() {
+        void 팝업_목록_조회에_성공한다() {
             // given
-            PopupWithChoicesCreateRequest popupWithChoicesCreateRequest =
-                    createPopupWithChoicesCreateRequest();
-            popupService.createPopup(popupWithChoicesCreateRequest);
-            popupService.createPopup(popupWithChoicesCreateRequest);
+            createPopup();
+            createPopup();
 
             // when
             List<PopupPreviewResponse> popupPreviewResponseList = popupService.findAllPopups();
 
             // then
             Assertions.assertAll(
-                    () -> assertThat(popupPreviewResponseList).hasSize(2),
-                    () -> assertThat(popupPreviewResponseList.get(0).popupId()).isEqualTo(2L),
-                    () -> assertThat(popupPreviewResponseList.get(1).popupId()).isEqualTo(1L));
+                    () -> assertThat(popupPreviewResponseList).hasSize(2), // 기존 팝업 + 새로 생성된 팝업
+                    () -> assertThat(popupPreviewResponseList.get(0).popupId()).isNotNull(),
+                    () -> assertThat(popupPreviewResponseList.get(1).popupId()).isNotNull());
+        }
+    }
+
+    @Nested
+    class 팝업_삭제 {
+        @Test
+        @Transactional
+        void 정상적으로_팝업을_삭제한다() {
+            // given
+            Long popupId = createPopup();
+
+            // when
+            popupService.deletePopup(popupId);
+
+            // then
+            assertThat(popupRepository.findById(popupId)).isEmpty();
+        }
+
+        @Test
+        @Transactional
+        void 존재하지_않는_팝업을_삭제하면_예외가_발생한다() {
+            // given
+            final Long nonExistentPopupId = 9999L;
+
+            // when & then
+            assertThatThrownBy(() -> popupService.deletePopup(nonExistentPopupId))
+                    .isInstanceOf(CustomException.class)
+                    .hasFieldOrPropertyWithValue("errorCode", PopupErrorCode.POPUP_NOT_FOUND);
+        }
+
+        @Test
+        @Transactional
+        void 권한이_없는_사용자가_팝업을_삭제하면_예외가_발생한다() {
+            // given
+            final Long popupId = createPopup();
+
+            setAuthentication(otherManager);
+
+            // when & then
+            assertThatThrownBy(() -> popupService.deletePopup(popupId))
+                    .isInstanceOf(CustomException.class)
+                    .hasFieldOrPropertyWithValue("errorCode", PopupErrorCode.POPUP_UNAUTHORIZED);
         }
     }
 
@@ -133,5 +180,10 @@ public class PopupServiceTest extends IntegrationTest {
                                 List.of("choice1", "choice2", "choice3", "choice4")),
                         new ChoiceCreateRequest(
                                 List.of("choice1", "choice2", "choice3", "choice4"))));
+    }
+
+    private Long createPopup() {
+        PopupCreateResponse popup = popupService.createPopup(createPopupWithChoicesCreateRequest());
+        return popup.popupId();
     }
 }

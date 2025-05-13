dc9c8e4475ac5154e8fc285726a38bd33d0cd255
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-05-13 16:12:33 +0900

    [LCR-136] feat: PopupEnter Entity 및 연관된 UserGender Enum 구현

---
+++ src/main/java/com/lgcns/domain/popupAccess/domain/PopupEnter.java
@@ -0,0 +1,52 @@
+package com.lgcns.domain.popupAccess.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PopupEnter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "popup_enter_id")
+    private Long id;
+
+    private Long popupId;
+
+    @Enumerated(EnumType.STRING)
+    private UserGender gender;
+
+    private int ageGroup;
+
+    private LocalDate date;
+
+    private LocalTime time;
+
+    @Builder
+    private PopupEnter(
+            Long popupId, UserGender gender, int ageGroup, LocalDate date, LocalTime time) {
+        this.popupId = popupId;
+        this.gender = gender;
+        this.ageGroup = ageGroup;
+        this.date = date;
+        this.time = time;
+    }
+
+    public static PopupEnter createPopupEnter(
+            Long popupId, UserGender gender, int ageGroup, LocalDate date, LocalTime time) {
+        return PopupEnter.builder()
+                .popupId(popupId)
+                .gender(gender)
+                .ageGroup(ageGroup)
+                .date(date)
+                .time(time)
+                .build();
+    }
+}

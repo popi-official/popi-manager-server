74884c1d49b4df04c508ab567b8cacd6bbcb9c7f
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-05-13 20:48:41 +0900

    [LCR-136] refactor: null 유효성 검사 위한 Integer 타입 변경

--- src/main/java/com/lgcns/domain/popupAccess/dto/request/PopupEnterCreateRequest.java
+++ src/main/java/com/lgcns/domain/popupAccess/dto/request/PopupEnterCreateRequest.java
@@ -10,14 +10,14 @@ public record PopupEnterCreateRequest(
         @NotNull(message = "방문자 성별은 필수입니다.") @Schema(description = "방문자 성별", example = "FEMALE")
                 UserGender gender,
         @NotNull(message = "방문자 나이대는 필수입니다.") @Schema(description = "방문자 나이대", example = "20")
-                int ageGroup,
+                Integer ageGroup,
         @NotNull(message = "예약 날짜는 필수입니다.")
                 @Schema(description = "팝업 예약 날짜", example = "2025-05-13")
                 LocalDate date,
         @NotNull(message = "예약 시간은 필수입니다.") @Schema(description = "팝업 예약 시간", example = "10:00:00")
                 LocalTime time) {
     public static PopupEnterCreateRequest of(
-            UserGender gender, int ageGroup, LocalDate date, LocalTime time) {
+            UserGender gender, Integer ageGroup, LocalDate date, LocalTime time) {
         return new PopupEnterCreateRequest(gender, ageGroup, date, time);
     }
 }

f2e76dbf2e4f539f06d4c8a9a46b5c9669529df9
현태 최 <ht3064@naver.com>
2025-05-14 21:35:10 +0900

    [LCR-143] feat: 상품 데이터 업데이트 flyway sql 파일 작성

---
+++ src/main/resources/db/migration/V10_4__update_item_data.sql
@@ -0,0 +1,31 @@
+UPDATE item
+SET
+    name = "데이즈드 DAZED A형 (지수) : 1월 [2024]",
+    price = 15000,
+    image_url = 'https://ygselect.com/web/product/big/202402/P0000NMY.jpg',
+    updated_at = NOW()
+WHERE item_id = 1;
+
+UPDATE item
+SET
+    name = "DAZED C형 (로제) : 스프링 에디션 [2024]",
+    price = 15000,
+    image_url = 'https://ygselect.com/web/product/big/202404/257d66b0a1eca57af67b6c792e68ed75.jpg',
+    updated_at = NOW()
+WHERE item_id = 2;
+
+UPDATE item
+SET
+    name = "DAZED A형 (제니) : 홀리데이 에디션 [2021]",
+    price = 9500,
+    image_url = 'https://image.aladin.co.kr/product/28453/14/cover500/k572835617_1.jpg',
+    updated_at = NOW()
+WHERE item_id = 3;
+
+UPDATE item
+SET
+    name = "DAZED A형 (리사) : [2019]",
+    price = 8000,
+    image_url = 'https://image.aladin.co.kr/product/17920/59/cover500/k142534034_1.jpg',
+    updated_at = NOW()
+WHERE item_id = 4;

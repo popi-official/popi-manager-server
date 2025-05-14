6c922455586748d21870511301ea5cadeb46a52b
jeongyeon0208 <githubb0208@gmail.com>
2025-05-14 21:57:52 +0900

    [LCR-141] refactor: ReservationStatsResponse @Schema 추가

--- src/main/java/com/lgcns/domain/reservationStats/dto/response/ReservationStatsResponse.java
+++ src/main/java/com/lgcns/domain/reservationStats/dto/response/ReservationStatsResponse.java
@@ -1,9 +1,12 @@
 package com.lgcns.domain.reservationStats.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
 public record ReservationStatsResponse(
-        Integer reservedCount, List<WeekDayReservationCountResponse> chart) {
+        @Schema(description = "금일 예약 수", example = "100") Integer reservedCount,
+        @Schema(description = "요일별 예약 수", example = "[{'day': '월요일', 'reservedCount': 10},...]")
+                List<WeekDayReservationCountResponse> chart) {
 
     public static ReservationStatsResponse of(
             Integer reservedCount, List<WeekDayReservationCountResponse> chart) {

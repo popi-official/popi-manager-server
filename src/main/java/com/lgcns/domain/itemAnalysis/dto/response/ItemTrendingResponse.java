f7e7a63e2cc8445b350c04cd56285e904a568b51
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-16 03:38:41 +0900

    [LCR-149] feat: API 구현

--- src/main/java/com/lgcns/domain/itemAnalysis/dto/response/ItemTrendingResponse.java
+++ src/main/java/com/lgcns/domain/itemAnalysis/dto/response/ItemTrendingResponse.java
@@ -1,9 +1,14 @@
 package com.lgcns.domain.itemAnalysis.dto.response;
 
 import com.lgcns.domain.item.domain.Item;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ItemTrendingResponse(
-        Long itemId, String title, String imagePath, Integer price, Integer stock) {
+        @Schema(description = "상품 ID", example = "1") Long itemId,
+        @Schema(description = "상품명", example = "POSTER SET") String title,
+        @Schema(description = "상품 이미지 URL", example = "https://bucket/asdf") String imagePath,
+        @Schema(description = "상품 가격", example = "50000") Integer price,
+        @Schema(description = "상품 재고", example = "100") Integer stock) {
     public static ItemTrendingResponse from(Item item) {
         return new ItemTrendingResponse(
                 item.getId(), item.getName(), item.getImageUrl(), item.getPrice(), item.getStock());

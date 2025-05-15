1dc5922d7377b59cfc554e88cdc2a01c64312fb5
woogieon8on <swp0927@naver.com>
2025-05-15 13:47:06 +0900

    [LCR-139] feat: 1번 팝업 Choice update 쿼리 작성

---
+++ src/main/resources/db/migration/V11_1__update_choice_content_data.sql
@@ -0,0 +1,95 @@
+UPDATE choice
+SET
+    content = "포스터"
+WHERE
+    choice_id = 1;
+
+UPDATE choice
+SET
+    content = "피규어"
+WHERE
+    choice_id = 2;
+
+UPDATE choice
+SET
+    content = "포토카드"
+WHERE
+    choice_id = 3;
+
+UPDATE choice
+SET
+    content = "키링"
+WHERE
+    choice_id = 4;
+
+UPDATE choice
+SET
+    content = "공식 SNS 채널"
+WHERE
+    choice_id = 5;
+
+UPDATE choice
+SET
+    content = "지인의 추천/공유"
+WHERE
+    choice_id = 6;
+
+UPDATE choice
+SET
+    content = "팬 커뮤니티"
+WHERE
+    choice_id = 7;
+
+UPDATE choice
+SET
+    content = "거리 광고 및 온라인 배너"
+WHERE
+    choice_id = 8;
+
+UPDATE choice
+SET
+    content = "공식 굿즈를 직접 보고 구매하고 싶어서"
+WHERE
+    choice_id = 9;
+
+UPDATE choice
+SET
+    content = "이번 팝업 공식 굿즈를 놓치고 싶지 않아서"
+WHERE
+    choice_id = 10;
+
+UPDATE choice
+SET
+    content = "친구, 팬 지인과 함께 팬활동을 즐기기 위해"
+WHERE
+    choice_id = 11;
+
+UPDATE choice
+SET
+    content = "단순 호기심"
+WHERE
+    choice_id = 12;
+
+UPDATE choice
+SET
+    content = "디자인 및 퀄리티"
+WHERE
+    choice_id = 13;
+
+UPDATE choice
+SET
+    content = "가격 대비 만족도"
+WHERE
+    choice_id = 14;
+
+UPDATE choice
+SET
+    content = "한정판 여부 및 희소성"
+WHERE
+    choice_id = 15;
+
+UPDATE choice
+SET
+    content = "실용성"
+WHERE
+    choice_id = 16;

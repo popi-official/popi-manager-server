5525ef8cbbfd7874c6c1e3695f5743e70fca1093
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-16 03:17:47 +0900

    [LCR-149] feat: API 응답 Response 구현

--- src/main/java/com/lgcns/domain/itemAnalysis/dto/response/ItemSalesStatsResponse.java
+++
@@ -1,9 +0,0 @@
-package com.lgcns.domain.itemAnalysis.dto.response;
-
-import com.lgcns.domain.itemAnalysis.domain.ItemSalesStats;
-
-public record ItemSalesStatsResponse(Long itemId, int salesVolume) {
-    public static ItemSalesStatsResponse from(ItemSalesStats entity) {
-        return new ItemSalesStatsResponse(entity.getItemId(), entity.getSalesVolume());
-    }
-}
--- src/main/java/com/lgcns/domain/itemAnalysis/dto/response/ItemTrendingResponse.java
+++ src/main/java/com/lgcns/domain/itemAnalysis/dto/response/ItemTrendingResponse.java
@@ -1,3 +1,11 @@
 package com.lgcns.domain.itemAnalysis.dto.response;
 
-public record ItemTrendingResponse() {}
+import com.lgcns.domain.item.domain.Item;
+
+public record ItemTrendingResponse(
+        Long itemId, String title, String imagePath, Integer price, Integer stock) {
+    public static ItemTrendingResponse from(Item item) {
+        return new ItemTrendingResponse(
+                item.getId(), item.getName(), item.getImageUrl(), item.getPrice(), item.getStock());
+    }
+}

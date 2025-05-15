43f7d3a47679778ce9d65a11dc2e3e0a60a0f6b4
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-15 16:20:24 +0900

    [LCR-149] chore: item_analysis 테이블 생성 스크립트 추가

---
+++ src/main/resources/db/migration/V13__create_item_analysis_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE item_analysis (
+    item_id BIGINT PRIMARY KEY,
+    popularity_score INT,
+    pre_survey_popularity DOUBLE,
+    sales_volume INT,
+    CONSTRAINT fk_item_analysis_item FOREIGN KEY (item_id) REFERENCES item(item_id) ON DELETE CASCADE
+);

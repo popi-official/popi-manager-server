0236bc0aaef2e280590ba8c5eebdc16543343083
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-16 01:26:28 +0900

    [LCR-149] feat: 상품판매량 엔티티 구현

---
+++ src/main/java/com/lgcns/domain/itemAnalysis/domain/ItemSalesStats.java
@@ -0,0 +1,39 @@
+package com.lgcns.domain.itemAnalysis.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemSalesStats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_sales_stats_id")
+    private Long id;
+
+    private Long popupId;
+
+    private Long itemId;
+
+    private int salesVolume = 0;
+
+    @Builder
+    private ItemSalesStats(Long popupId, Long itemId, int salesVolume) {
+        this.popupId = popupId;
+        this.itemId = itemId;
+        this.salesVolume = salesVolume;
+    }
+
+    public static ItemSalesStats createItemSalesStats(Long popupId, Long itemId, int salesVolume) {
+        return ItemSalesStats.builder()
+                .popupId(popupId)
+                .itemId(itemId)
+                .salesVolume(salesVolume)
+                .build();
+    }
+}

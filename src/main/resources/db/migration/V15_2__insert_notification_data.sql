b3fb234d425699cd3688f78a96a74e9f202018fe
jeongyeon0208 <githubb0208@gmail.com>
2025-05-16 10:38:16 +0900

    [LCR-146] refactor: flyway 스크립트 수정

--- src/main/resources/db/migration/V15_2__insert_notification_data.sql
+++ src/main/resources/db/migration/V15_2__insert_notification_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO notification (manager_id, popup_id, item_id, item_name, popularity, min_stock, created_at, updated_at) VALUES
-        (1, 1, 11, 'COLLECTIBLE FIGURE_JENNIE', 'hot', 250, NOW(), NOW()),
-        (1, 1, 5,  '[PHOTOCARD] BACK TO RETRO','hot', 100, NOW(), NOW()),
-        (1, 1, 17, 'KRUNK X MINI CROSS BAG', 'normal', 100, NOW(), NOW()),
-        (1, 1, 15, '더 게임 OST [THE GIRLS] Stella ver. (LIMITED EDITION)', 'normal', 180, NOW(), NOW()),
-        (1, 1, 6,  '[PHOTOCARD] LOVELY VALENTINES EDITION', 'normal', 200, NOW(), NOW()),
-        (1, 1, 22, 'KRUNK X KEYRING', 'normal', 80 , NOW(), NOW()),
-        (1, 1, 7,  '[PHOTOCARD] CHRISTMAS EDITION','normal', 150, NOW(), NOW()),
-        (1, 1, 8,  '[PHOTOCARD] No.4~6 (SET)', 'normal', 200, NOW(), NOW());
+        (1, 1, 11, 'FIGURE_JENNIE', 'hot', 250, "2025-05-16T10:31:02", NOW()),
+        (1, 1, 5,  'BACK TO RETRO','normal', 50, "2025-05-16T09:48:49", NOW()),
+        (1, 1, 17, 'MINI CROSS BAG', 'hot', 100, "2025-05-15T17:58:30", NOW()),
+        (1, 1, 15, '더 게임 OST', 'normal', 180, "2025-05-15T14:15:20", NOW()),
+        (1, 1, 6,  'LOVELY VALENTINES', 'normal', 200, "2025-05-14T12:41:10", NOW()),
+        (1, 1, 22, 'KRUNK X KEYRING', 'hot', 80 , "2025-05-13T15:33:29", NOW()),
+        (1, 1, 7,  'CHRISTMAS EDITION','normal', 150, "2025-05-13T10:19:51", NOW()),
+        (1, 1, 8,  'No.4~6 (SET)', 'normal', 200, "2025-05-13T16:44:12", NOW());

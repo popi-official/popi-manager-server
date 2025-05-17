6dba92a18bd94fdf52dd442f8e1446654c2a2876
jeongyeon0208 <githubb0208@gmail.com>
2025-05-17 10:07:00 +0900

    [LCR-156] refactor: 알림 내 상품명 수정 flyway 스크립트 작성

---
+++ src/main/resources/db/migration/V17_2__update_notification_data.sql
@@ -0,0 +1,8 @@
+UPDATE notification SET item_name = '피규어 제니' WHERE item_id = 11;
+UPDATE notification SET item_name = '포토카드 RETRO' WHERE item_id = 5;
+UPDATE notification SET item_name = '크룽크 미니백' WHERE item_id = 17;
+UPDATE notification SET item_name = 'OST Stella' WHERE item_id = 15;
+UPDATE notification SET item_name = '포토카드 VAL' WHERE item_id = 6;
+UPDATE notification SET item_name = '크룽크 키링' WHERE item_id = 22;
+UPDATE notification SET item_name = '포토카드 XMAS' WHERE item_id = 7;
+UPDATE notification SET item_name = '포토카드 SET' WHERE item_id = 8;

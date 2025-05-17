d45605005593f0c2ef1a8ff3b7070dd7d8d37ef9
jeongyeon0208 <githubb0208@gmail.com>
2025-05-17 10:47:27 +0900

    [LCR-156] refactor: 아이템 이름 수정 flyway 스크립트 작성

---
+++ src/main/resources/db/migration/V17_3__update_item_name_data.sql
@@ -0,0 +1,29 @@
+UPDATE item SET name = 'DAZED 지수' WHERE item_id = 1;
+UPDATE item SET name = 'DAZED 로제' WHERE item_id = 2;
+UPDATE item SET name = 'DAZED 제니' WHERE item_id = 3;
+UPDATE item SET name = 'DAZED 리사' WHERE item_id = 4;
+
+UPDATE item SET name = '포토카드 RETRO' WHERE item_id = 5;
+UPDATE item SET name = '포토카드 VAL' WHERE item_id = 6;
+UPDATE item SET name = '포토카드 XMAS' WHERE item_id = 7;
+UPDATE item SET name = '포토카드 SET' WHERE item_id = 8;
+
+UPDATE item SET name = '피규어 지수' WHERE item_id = 9;
+UPDATE item SET name = '피규어 로제' WHERE item_id = 10;
+UPDATE item SET name = '피규어 제니' WHERE item_id = 11;
+UPDATE item SET name = '피규어 리사' WHERE item_id = 12;
+
+UPDATE item SET name = '포스터 세트' WHERE item_id = 13;
+UPDATE item SET name = '콘서트 스탠드' WHERE item_id = 14;
+UPDATE item SET name = 'OST Stella' WHERE item_id = 15;
+UPDATE item SET name = '슈퍼스타즈' WHERE item_id = 16;
+
+UPDATE item SET name = '크룽크 미니백' WHERE item_id = 17;
+UPDATE item SET name = '크룽크 라운드백' WHERE item_id = 18;
+UPDATE item SET name = '크룽크 버니햇' WHERE item_id = 19;
+UPDATE item SET name = '크룽크 쿠션' WHERE item_id = 20;
+
+UPDATE item SET name = '크룽크 손목인형' WHERE item_id = 21;
+UPDATE item SET name = '크룽크 키링' WHERE item_id = 22;
+UPDATE item SET name = '크룽크 미니쿠션' WHERE item_id = 23;
+UPDATE item SET name = '크룽크 빅쿠션' WHERE item_id = 24;

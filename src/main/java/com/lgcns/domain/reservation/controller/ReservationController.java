a292ce51aef89d9896f0c38816e255c9a328cf27
jeongyeon0208 <githubb0208@gmail.com>
2025-05-21 17:16:33 +0900

    [LCR-188] feat: raw response 반환용 custom annotation 적용

--- src/main/java/com/lgcns/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/lgcns/domain/reservation/controller/ReservationController.java
@@ -2,6 +2,7 @@ package com.lgcns.domain.reservation.controller;
 
 import com.lgcns.domain.reservation.dto.MonthlyReservationResponse;
 import com.lgcns.domain.reservation.service.ReservationService;
+import com.lgcns.global.common.annotation.RawResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,7 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @GetMapping("/popups/{popupId}")
+    @RawResponse
     @Operation(summary = "예약 조회", description = "팝업스토어 ID와 달을 통해 예약 정보를 조회합니다.")
     public MonthlyReservationResponse reservationListFind(
             @PathVariable Long popupId, @RequestParam String date) {

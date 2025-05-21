e85077c1ec5bcc79058ece5cf6d19b22ff159c68
jeongyeon0208 <githubb0208@gmail.com>
2025-05-21 17:44:17 +0900

    [LCR-188] refactor: controller 메서드명 수정

--- src/main/java/com/lgcns/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/lgcns/domain/reservation/controller/ReservationController.java
@@ -19,7 +19,7 @@ public class ReservationController {
     @GetMapping("/popups/{popupId}")
     @RawResponse
     @Operation(summary = "해당 달에 대한 예약 목록 조회", description = "팝업스토어 ID와 달을 통해 예약 정보를 조회합니다.")
-    public MonthlyReservationResponse reservationListFind(
+    public MonthlyReservationResponse userReservationListFind(
             @PathVariable Long popupId, @RequestParam String date) {
         return reservationService.findReservationByIdAndDate(popupId, date);
     }

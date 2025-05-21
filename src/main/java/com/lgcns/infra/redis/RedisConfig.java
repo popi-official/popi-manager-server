a7b511a70eb5c7bc6031c3dd5f38d41b09681997
jeongyeon0208 <githubb0208@gmail.com>
2025-05-21 12:43:13 +0900

    [LCR-180] chore: user redis 연결 config, properties 설정

--- src/main/java/com/lgcns/infra/redis/RedisConfig.java
+++ src/main/java/com/lgcns/infra/redis/RedisConfig.java
@@ -2,14 +2,22 @@ package com.lgcns.infra.redis;
 
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories(
+        basePackages = "com.lgcns.domain.auth.repository",
+        redisTemplateRef = "redisTemplate")
 @RequiredArgsConstructor
 public class RedisConfig {
 
@@ -30,5 +38,16 @@ public class RedisConfig {
                         .build();
 
         return new LettuceConnectionFactory(redisStandaloneConfig, lettuceClientConfig);
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(
+            @Qualifier("redisConnectionFactory") RedisConnectionFactory factory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
     }
 }

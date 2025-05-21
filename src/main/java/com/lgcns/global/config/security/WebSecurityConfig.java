e8c8136a603517a10939f41b3888f3f49d4b8d38
jeongyeon0208 <githubb0208@gmail.com>
2025-05-21 17:27:28 +0900

    [LCR-188] feat: WebSecurityConfig reservation permitAll 적용

--- src/main/java/com/lgcns/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/lgcns/global/config/security/WebSecurityConfig.java
@@ -64,6 +64,8 @@ public class WebSecurityConfig {
                                 .permitAll()
                                 .requestMatchers("/entrances")
                                 .permitAll()
+                                .requestMatchers("/reservations/**")
+                                .permitAll()
                                 .anyRequest()
                                 .authenticated());
 

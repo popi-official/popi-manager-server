79a977a5026dce0dfb93c92ca1c29760813fea86
조영상 <51320595+0ssang@users.noreply.github.com>
2025-05-22 17:46:45 +0900

    [LCR-193] feat: 예약 가능한 팝업 리스트 조회 쿼리 구현

--- src/main/java/com/lgcns/domain/popup/repository/PopupRepositoryCustom.java
+++ src/main/java/com/lgcns/domain/popup/repository/PopupRepositoryCustom.java
@@ -1,9 +1,13 @@
 package com.lgcns.domain.popup.repository;
 
+import com.lgcns.domain.popup.dto.response.PopupInfoResponse;
 import com.lgcns.domain.popup.dto.response.PopupPreviewResponse;
 import java.util.List;
+import org.springframework.data.domain.Slice;
 
 public interface PopupRepositoryCustom {
 
     List<PopupPreviewResponse> findAllPopupsByManagerId(Long managerId);
+
+    Slice<PopupInfoResponse> findAllActivePopups(Long lastPopupId, int size);
 }

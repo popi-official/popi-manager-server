0ddd5ee600b86599363884be44b8a8653701e31a
woogieon8on <swp0927@naver.com>
2025-05-27 00:08:23 +0900

    [LCR-209] refactor: ItemInternalController 메서드명 수정

--- src/main/java/com/lgcns/domain/item/internalApi/ItemInternalController.java
+++ src/main/java/com/lgcns/domain/item/internalApi/ItemInternalController.java
@@ -24,7 +24,7 @@ public class ItemInternalController {
             description =
                     "keyword를 포함하지 않으면 현재 팝업의 모든 상품을 무한 스크롤을 위하여 페이징 처리한 뒤 반환합니다.</br>"
                             + "keyword를 포함하여 호출하면 상품명에 keyword가 포함된 모든 상품을 페이징 처리한 뒤 반환합니다.")
-    public SliceResponse<ItemInfoResponse> userItemFindAll(
+    public SliceResponse<ItemInfoResponse> itemFindByName(
             @Parameter(description = "팝업 ID", example = "1") @PathVariable(name = "popupId")
                     Long popupId,
             @Parameter(description = "검색할 상품 이름 (비워두면 모든 상품을 반환합니다.)", example = "블랙핑크")
@@ -41,7 +41,7 @@ public class ItemInternalController {
 
     @GetMapping("/default")
     @Operation(summary = "기본 상품 목록 조회", description = "무작위하게 선택된 4개의 상품을 조회합니다.")
-    public List<ItemInfoResponse> useItemFindDefault(
+    public List<ItemInfoResponse> itemFindDefault(
             @Parameter(description = "팝업 ID", example = "1") @PathVariable(name = "popupId")
                     Long popupId) {
         return itemService.findRandomItems(popupId);

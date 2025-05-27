87c36173d7e8bab6cc365ea637aaac568117f641
jeongyeon0208 <githubb0208@gmail.com>
2025-05-27 16:35:30 +0900

    [LCR-219] feat: reservation 정보 조회 Internal API 구현

--- src/main/java/com/lgcns/domain/reservation/service/ReservationService.java
+++ src/main/java/com/lgcns/domain/reservation/service/ReservationService.java
@@ -1,8 +1,11 @@
 package com.lgcns.domain.reservation.service;
 
 import com.lgcns.domain.reservation.dto.response.MonthlyReservationResponse;
+import com.lgcns.domain.reservation.dto.response.ReservationInfoResponse;
 
 public interface ReservationService {
 
     MonthlyReservationResponse findReservationByIdAndDate(Long popupId, String date);
+
+    ReservationInfoResponse findReservationById(Long reservationId);
 }

d1f15f7df727cbc727bdd90f67eeeb7ccd56e53e
woogieon8on <swp0927@naver.com>
2025-05-28 15:58:58 +0900

    [LCR-238] chore: WireMock 테스트 설정 추가

--- src/test/java/com/lgcns/IntegrationTest.java
+++ src/test/java/com/lgcns/IntegrationTest.java
@@ -1,10 +1,13 @@
 package com.lgcns;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import com.lgcns.domain.manager.domain.Manager;
 import com.lgcns.global.security.PrincipalDetails;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,12 +15,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureWireMock(port = 0)
 public abstract class IntegrationTest {
     @Autowired protected DatabaseCleaner databaseCleaner;
+    @Autowired private WireMockServer wireMockServer;
 
     @BeforeEach
     void setUp() {
         databaseCleaner.execute();
+
+        wireMockServer.stop();
+        wireMockServer.start();
+    }
+
+    @AfterEach
+    void afterEach() {
+        wireMockServer.resetAll();
     }
 
     // 지정된 관리자로 로그인 상태로 변경

eb2d0b6034a40a8da4084903c091bed9e3ba4fbd
Hyuntaei Choi <145987233+ht3064@users.noreply.github.com>
2025-05-30 17:43:09 +0900

    chore: Kafka 메시지 수신을 위한 Consumer 설정 추가 (#80)
    * [LCR-275] chore: kafka 의존성 추가
    
    * [LCR-275] chore: local, test.yml에 kafka bootstrap-servers 추가
    
    * [LCR-275] chore: Kafka Consumer 설정 클래스 추가
    
    * [LCR-275] chore: 로컬 Kafka 실행용 docker-compose-kafka.yml 추가
    
    * [LCR-275] fix: LOCAL_TEST_URL 오타 수정
    
    * [LCR-275] chore: kafka 포트 localhost:29092로 수정

--- src/main/java/com/lgcns/global/common/constants/UrlConstants.java
+++ src/main/java/com/lgcns/global/common/constants/UrlConstants.java
@@ -3,7 +3,7 @@ package com.lgcns.global.common.constants;
 public final class UrlConstants {
     public static final String DEV_CLIENT_URL = "https://www.dev.ceo.popi.today";
     public static final String LOCAL_CLIENT_URL = "http://localhost:3000";
-    public static final String LOCAL_TEST_URL = "- http://localhost:4173";
+    public static final String LOCAL_TEST_URL = "http://localhost:4173";
 
     public static final String DEV_SERVER_URL = "https://dev-api.ceo.popi.today";
     public static final String LOCAL_SERVER_URL = "http://localhost:8080";

ecc4eef91dea069ecad327872af4389a03a4e297
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-06-02 22:08:57 +0900

    [LCR-280] feat: MemberAnswer 메시지 저장 위한 Kafka Consumer 구현

---
+++ src/main/java/com/lgcns/domain/survey/kafka/message/MemberAnswerMessage.java
@@ -0,0 +1,10 @@
+package com.lgcns.domain.survey.kafka.message;
+
+import com.lgcns.domain.survey.kafka.dto.SurveyChoiceDto;
+import java.util.List;
+
+public record MemberAnswerMessage(Long memberId, List<SurveyChoiceDto> surveyChoices) {
+    public static MemberAnswerMessage of(Long memberId, List<SurveyChoiceDto> surveyChoices) {
+        return new MemberAnswerMessage(memberId, surveyChoices);
+    }
+}

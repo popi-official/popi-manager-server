b4b4c24b991e0aea7a6ad43bf4419be726950a82
jeongyeon0208 <githubb0208@gmail.com>
2025-06-02 15:31:09 +0900

    [LCR-294] refactor: DTO로 메세지 수신받게 수정

--- src/main/java/com/lgcns/domain/entrance/kafka/consumer/MemberEnteredConsumer.java
+++ src/main/java/com/lgcns/domain/entrance/kafka/consumer/MemberEnteredConsumer.java
@@ -1,6 +1,5 @@
 package com.lgcns.domain.entrance.kafka.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lgcns.domain.entrance.domain.Entrance;
 import com.lgcns.domain.entrance.kafka.message.MemberEnteredMessage;
 import com.lgcns.domain.entrance.repository.EntranceRepository;
@@ -14,28 +13,24 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class MemberEnteredConsumer {
 
+    private static final String TOPIC = "member-entered-topic";
+
     private final EntranceRepository entranceRepository;
-    private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = "member-entered-topic", groupId = "entrance-group")
-    public void createEntrance(String message) {
-        try {
-            MemberEnteredMessage kafkaMessage =
-                    objectMapper.readValue(message, MemberEnteredMessage.class);
+    @KafkaListener(
+            topics = TOPIC,
+            groupId = "member-entered",
+            containerFactory = "memberEnteredMessageConcurrentKafkaListenerContainerFactory")
+    public void createEntrance(MemberEnteredMessage message) {
 
-            Entrance entrance =
-                    Entrance.createPopupEnter(
-                            kafkaMessage.popupId(),
-                            kafkaMessage.gender(),
-                            kafkaMessage.age(),
-                            kafkaMessage.reservationDate(),
-                            kafkaMessage.reservationTime());
+        Entrance entrance =
+                Entrance.createPopupEnter(
+                        message.popupId(),
+                        message.gender(),
+                        message.age(),
+                        message.reservationDate(),
+                        message.reservationTime());
 
-            entranceRepository.save(entrance);
-
-        } catch (Exception e) {
-            // 로그만 남기거나 DLQ 처리 등
-            log.error("[Kafka] 메시지 파싱 실패: {}", message, e);
-        }
+        entranceRepository.save(entrance);
     }
 }

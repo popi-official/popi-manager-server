9a52a5f3e260195e66024008dd300a8be3d2c912
HyungSuhOh <43169705+ohhyungsuh@users.noreply.github.com>
2025-06-03 15:57:07 +0900

    [LCR-280] feat: surveyId를 통해 Choice 찾는 JPA 추가

--- src/main/java/com/lgcns/domain/survey/repository/ChoiceRepository.java
+++ src/main/java/com/lgcns/domain/survey/repository/ChoiceRepository.java
@@ -1,6 +1,9 @@
 package com.lgcns.domain.survey.repository;
 
 import com.lgcns.domain.survey.domain.Choice;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChoiceRepository extends JpaRepository<Choice, Long> {}
+public interface ChoiceRepository extends JpaRepository<Choice, Long> {
+    List<Choice> findBySurveyId(Long surveyId);
+}

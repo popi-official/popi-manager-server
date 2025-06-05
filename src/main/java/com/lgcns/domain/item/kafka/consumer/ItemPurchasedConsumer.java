b90f783455b9e6512eb2a29667ea49220b06369f
현태 최 <ht3064@naver.com>
2025-06-06 03:27:54 +0900

    [LCR-307] feat: 상품 구매 시 재고 부족 시점에 WebSocket 알림 전송 기능 추가

--- src/main/java/com/lgcns/domain/item/kafka/consumer/ItemPurchasedConsumer.java
+++ src/main/java/com/lgcns/domain/item/kafka/consumer/ItemPurchasedConsumer.java
@@ -2,6 +2,7 @@ package com.lgcns.domain.item.kafka.consumer;
 
 import com.lgcns.domain.item.kafka.message.ItemPurchasedMessage;
 import com.lgcns.domain.item.repository.ItemRepository;
+import com.lgcns.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ public class ItemPurchasedConsumer {
     private static final String TOPIC = "item-purchased-topic";
 
     private final ItemRepository itemRepository;
+    private final NotificationService notificationService;
 
     @KafkaListener(
             topics = TOPIC,
@@ -21,11 +23,16 @@ public class ItemPurchasedConsumer {
     public void decreaseStock(ItemPurchasedMessage message) {
         for (ItemPurchasedMessage.Item item : message.items()) {
             itemRepository
-                    .findById(item.itemId())
+                    .findWithPopupAndManagerById(item.itemId())
                     .ifPresent(
                             foundItem -> {
                                 foundItem.decreaseStock(item.quantity());
                                 itemRepository.save(foundItem);
+
+                                if (foundItem.getStock() <= foundItem.getMinStock()) {
+                                    notificationService.sendLowStockMessage(
+                                            foundItem.getPopup(), foundItem);
+                                }
                             });
         }
     }
--- src/main/java/com/lgcns/domain/item/repository/ItemRepository.java
+++ src/main/java/com/lgcns/domain/item/repository/ItemRepository.java
@@ -1,6 +1,12 @@
 package com.lgcns.domain.item.repository;
 
 import com.lgcns.domain.item.domain.Item;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {}
+public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
+    @Query("select i from Item i join fetch i.popup p join fetch p.manager where i.id = :id")
+    Optional<Item> findWithPopupAndManagerById(@Param("id") Long id);
+}

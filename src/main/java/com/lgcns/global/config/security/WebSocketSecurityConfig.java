93cd7dcb07cac232a9861fa1b243bb7b110fe280
현태 최 <ht3064@naver.com>
2025-06-06 02:55:15 +0900

    [LCR-307] feat: WebSocket Spring Security 설정 클래스 추가

---
+++ src/main/java/com/lgcns/global/config/security/WebSocketSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.lgcns.global.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.nullDestMatcher()
+                .permitAll()
+                .simpSubscribeDestMatchers("/topic/**")
+                .authenticated()
+                .anyMessage()
+                .denyAll();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}

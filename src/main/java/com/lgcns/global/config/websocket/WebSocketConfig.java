772bff49876399265a9db532b9e05babdaa683bc
현태 최 <ht3064@naver.com>
2025-06-06 03:11:16 +0900

    [LCR-307] feat: JWT 인증 인터셉터 구현

--- src/main/java/com/lgcns/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/lgcns/global/config/websocket/WebSocketConfig.java
@@ -5,8 +5,10 @@ import static com.lgcns.global.common.constants.UrlConstants.LOCAL_CLIENT_URL;
 import static com.lgcns.global.helper.SpringEnvironmentHelper.DEV;
 
 import com.lgcns.global.helper.SpringEnvironmentHelper;
+import com.lgcns.global.websocket.JwtChannelInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -18,6 +20,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final SpringEnvironmentHelper springEnvironmentHelper;
+    private final JwtChannelInterceptor jwtChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -34,5 +37,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtChannelInterceptor);
     }
 }

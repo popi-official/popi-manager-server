be065acce65eb10331de3fca70c2f898cbbdd6c3
jeongyeon0208 <githubb0208@gmail.com>
2025-06-10 23:44:27 +0900

    [LCR-338] refactor: ConsumerFactory명 수정

--- src/main/java/com/lgcns/domain/entrance/kafka/config/MemberEnteredConsumerConfig.java
+++ src/main/java/com/lgcns/domain/entrance/kafka/config/MemberEnteredConsumerConfig.java
@@ -23,7 +23,7 @@ public class MemberEnteredConsumerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ConsumerFactory<String, MemberEnteredMessage> memberEnteredMessageConsumerFactory() {
+    public ConsumerFactory<String, MemberEnteredMessage> memberEnteredConsumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -44,7 +44,7 @@ public class MemberEnteredConsumerConfig {
             memberEnteredMessageConcurrentKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, MemberEnteredMessage>
                 kafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
-        kafkaListenerContainerFactory.setConsumerFactory(memberEnteredMessageConsumerFactory());
+        kafkaListenerContainerFactory.setConsumerFactory(memberEnteredConsumerFactory());
         return kafkaListenerContainerFactory;
     }
 }

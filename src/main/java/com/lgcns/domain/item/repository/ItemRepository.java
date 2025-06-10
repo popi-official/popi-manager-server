e90371e4c1f60f208d8ee4706b07b34963ff2b7b
jeongyeon0208 <githubb0208@gmail.com>
2025-06-10 16:57:15 +0900

    [LCR-295] refactor: 중복 아이템 조회 메서드 삭제

--- src/main/java/com/lgcns/domain/item/repository/ItemRepository.java
+++ src/main/java/com/lgcns/domain/item/repository/ItemRepository.java
@@ -2,8 +2,6 @@ package com.lgcns.domain.item.repository;
 
 import com.lgcns.domain.item.domain.Item;
 import java.util.Optional;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,9 +10,6 @@ import org.springframework.data.repository.query.Param;
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
     @Query("select i from Item i join fetch i.popup p join fetch p.manager where i.id = :id")
     Optional<Item> findWithPopupAndManagerById(@Param("id") Long id);
-
-    @Query("select i from Item i join fetch i.popup p join fetch p.manager where i.id = :id")
-    List<Item> findByPopupId(Long popupId);
 
     @Modifying
     @Query(

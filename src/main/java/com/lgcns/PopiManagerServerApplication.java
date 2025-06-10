4657516d67506d1baf70f0bbd18c73946c75ee9d
jeongyeon0208 <githubb0208@gmail.com>
2025-06-10 17:05:22 +0900

    [LCR-295] refactor: Async Config 분리

--- src/main/java/com/lgcns/PopiManagerServerApplication.java
+++ src/main/java/com/lgcns/PopiManagerServerApplication.java
@@ -2,10 +2,8 @@ package com.lgcns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class PopiManagerServerApplication {
 
     public static void main(String[] args) {

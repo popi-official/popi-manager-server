72a48a0f67a4fdcc83ef945d7828f3d0dd4196d3
조영상 <51320595+0ssang@users.noreply.github.com>
2025-06-11 09:31:20 +0900

    [LCR-321] feat: 커스텀 에러코드 정의

---
+++ src/main/java/com/lgcns/domain/reservationStats/exception/ReservationStatsErrorCode.java
@@ -0,0 +1,24 @@
+package com.lgcns.domain.reservationStats.exception;
+
+import com.lgcns.global.error.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public enum ReservationStatsErrorCode implements ErrorCode {
+    RESERVATION_SERVICE_CONNECTION_FAILED(HttpStatus.SERVICE_UNAVAILABLE, "예약 서비스에 연결할 수 없습니다."),
+    RESERVATION_SERVICE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예약 서비스에서 오류가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
